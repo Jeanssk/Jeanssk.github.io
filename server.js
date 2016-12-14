37aa6c1d31b1467c830469470aead4017f1f31db
Jeanssk <j.piessevaux@elium.academy>
2016-12-14 12:41:32 +0100

    personalizedsk

---
+++ server.js
@@ -0,0 +1,16 @@
+console.log("Jean's CV")
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(express.static('./'));
+
+// app.listen(3000, function() {
+//   console.log('listening on 3000')
+// })
+
+
+app.get('/', function(req,res) {
+	res.sendFile(__dirname+'/index.html')
+})
